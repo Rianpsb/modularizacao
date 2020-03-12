bd04aa9e1fa65f7478bea8f3737f182d5a31bea4
19180091 <19180091@DSN1271137817.cfp127aluno.local>
2020-03-12 11:17:29 -0300

    Modularizao Leds intercalados
    Botao e Leds

--- Modularizacao_Leds_intercados.X/Led_button.h
+++ Modularizacao_Leds_intercados.X/Led_button.h
@@ -1,10 +1,6 @@
-   
+#ifndef LED_BUTTON
+#define LED_BUTTON
 
-void Led_button (void)
-{
-    PORTD=0;
-    DIR_S1=ENTRADA;
-    DIR_S2=ENTRADA;
-    DIR_LED1=SAIDA;
-    DIR_LED2=SAIDA;
-}
+void led_button(void);
+
+#endif
